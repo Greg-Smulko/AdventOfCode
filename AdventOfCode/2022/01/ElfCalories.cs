d8ba2051761082dcc8bd2d7370bdac1ec5dd0e4f
Greg Smulko <Greg.Smulko@red-gate.com>
2022-12-01 21:36:27 +0000

    Add Day 1

---
+++ AdventOfCode/2022/01/ElfCalories.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode;
+
+public static class ElfCalories 
+{
+    public static double FindMaxTotalCalories()
+    {
+        double max = 0, total = 0;
+        var lines = File.ReadAllLines("2022/01/ElfCaloriesInput.txt");
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                if (total > max)
+                {
+                    max = total;
+                }
+                total = 0;
+                continue;
+            }
+
+            total += double.Parse(line);
+        }
+
+        return max;
+    }
+
+    public static double FindMaxThreeTotalCalories()
+    {
+        var totalPerElf = new List<double>();
+        double total = 0;
+        
+        var lines = File.ReadAllLines("2022/01/ElfCaloriesInput.txt");
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                totalPerElf.Add(total);
+                total = 0;
+                continue;
+            }
+
+            total += double.Parse(line);
+        }
+
+        return totalPerElf.OrderDescending().Take(3).Sum();
+    }
+}
---
+++ AdventOfCode/2022/01/ElfCaloriesTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode;
+
+public class ElfCaloriesTest
+{
+    [Test]
+    public void Test1()
+    {
+        var result = ElfCalories.FindMaxTotalCalories();
+        result.Should().BeNegative();
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var result = ElfCalories.FindMaxThreeTotalCalories();
+        result.Should().BeNegative();
+    }
+}
